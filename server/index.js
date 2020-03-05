dd09ac4d2b5bca92c67e008c5342ef98459f109f
Reza <rzgholizadeh@gmail.com>
2020-03-05 16:27:26 -0700

    refactor: Change the message shown to user

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 //  a heelo world rout handler
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "guys" });
 });
 
 // Setting the port deynamically (for Herouko), or 5000 for local machine
