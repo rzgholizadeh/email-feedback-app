107523c966c177b87b89432123eb5163ac72b9d0
Reza <rzgholizadeh@gmail.com>
2020-03-08 01:23:52 -0700

    chore: Create dev and prod environments

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// production keys are here
+// do commit this one
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
