600da7417248958e214b9362d077d49f037028ac
Reza <rzgholizadeh@gmail.com>
2020-03-08 17:11:07 -0600

    feat: Add create-react-app project as client

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> Hi World
+        </p>
+        <a
+          className="App-link"
+          href="http://localhost:5000/auth/google"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sign in with Google
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
