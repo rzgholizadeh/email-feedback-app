1392c333b8e9f363dd7123913d4ee378bf47615b
Reza <rzgholizadeh@gmail.com>
2020-03-07 22:13:12 -0700

    feat: Add logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,9 +8,17 @@ module.exports = app => {
       scope: ["profile", "email"]
     })
   );
+  //routhandler
+
   // routhandler for the callbackURL - tell passport to handle the callback
   app.get("/auth/google/callback", passport.authenticate("google"));
-
+  // routhandler for logout
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    // tell user  a sign of logout (we show that user is empty)
+    res.send(req.user);
+  });
+  // routhandler to show the correctness of login result
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
