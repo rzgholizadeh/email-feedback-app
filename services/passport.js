564c797eae7247e87b742fdfbad6eefb9380c1fe
Reza <rzgholizadeh@gmail.com>
2020-03-08 18:35:15 -0600

    refactor: Updated the promises to await mechanism

--- services/passport.js
+++ services/passport.js
@@ -29,25 +29,22 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       // this is  where we use the information google has provided about the user
       // query the DB to see if the user already exists
       // this is an async action and we use promise to get the result
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // the user has been found in the DB
-          // first arg: error message
-          // second arg: the user we found in DB
-          done(null, existingUser);
-        } else {
-          // the user is new
-          // create a model instance of User and set its googleId, and save it to the database
-          // Async action, needs promise
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // the user has been found in the DB
+        // first arg: error message
+        // second arg: the user we found in DB
+        return done(null, existingUser);
+      }
+      // the user is new
+      // create a model instance of User and set its googleId, and save it to the database
+      // Async action, needs promise
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
