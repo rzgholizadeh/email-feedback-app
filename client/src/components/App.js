a41a90a728d7505ebc2be81eefcbdae56272e176
Reza <rzgholizadeh@gmail.com>
2020-03-08 22:03:35 -0600

    feat: Setup react router with dummy objects

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+// some dummy components to se how router works
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
