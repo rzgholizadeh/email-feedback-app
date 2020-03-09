28a92ed543262723e16e23890fe86e543e9c0818
Reza <rzgholizadeh@gmail.com>
2020-03-09 00:17:51 -0600

    refactor: Refactor the action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios
-      .get("/api/current_user")
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+export const fetchUser = () => async dispatch => {
+  dispatch({ type: FETCH_USER, payload: await axios.get("/api/current_user") });
 };
