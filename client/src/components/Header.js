106c9ee6249ceb5f1dd19f3ca99ae9a6e60cff8d
Reza <rzgholizadeh@gmail.com>
2020-03-09 01:13:35 -0600

    refactor: Develop the code for making the logo a  dynamic Link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
@@ -26,7 +27,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Email-Feedback</a>
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            Email-Feedback
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
