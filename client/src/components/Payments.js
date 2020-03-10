ded40be5e10777aee1f3286cc7e6d618e83e7da4
Reza <rzgholizadeh@gmail.com>
2020-03-09 20:32:27 -0600

    feat: Setup react-stripe-checkout component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,18 @@
+// This is for configuring react-stripe-checkout
+// This is a wrapper for that
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      />
+    );
+  }
+}
+
+export default Payments;
