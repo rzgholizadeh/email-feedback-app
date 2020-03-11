aa27bf6c54dd214d524a98e2689bcd53ad70be7d
Reza <rzgholizadeh@gmail.com>
2020-03-11 00:32:56 -0600

    feat: Develop the code for creating and sending the emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -5,10 +5,23 @@ const keys = require("../config/keys");
 class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email("no-reply@emailFeedback.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
+    // addContent is a built in function in helper.Mail
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
   }
 }
 
