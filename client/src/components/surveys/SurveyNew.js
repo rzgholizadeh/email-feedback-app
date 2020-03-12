a8ebf8d6bf113c536ef6979cefc878f579db87d2
Reza <rzgholizadeh@gmail.com>
2020-03-12 00:05:42 -0600

    feat: Create the form review component

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,25 @@
 // This contains survey form and survey form review
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
+  // component level state
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
+    } else {
+      return (
+        <SurveyForm
+          onSurveySubmit={() => this.setState({ showFormReview: true })}
+        />
+      );
+    }
+  }
   render() {
-    return (
-      <div>
-        <SurveyForm />
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
 
