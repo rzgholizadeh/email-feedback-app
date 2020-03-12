301d48adcc663e39de45be72d40ae97071e00adc
Reza <rzgholizadeh@gmail.com>
2020-03-11 18:59:19 -0600

    feat: Create dashboard component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ import * as actions from "../actions";
 // some dummy components to se how router works
 import Header from "./Header";
 import Landing from "./Landing";
-const Dashboard = () => <h2>Dashboard</h2>;
+import Dashboard from "./Dashboard";
 const SurveyNew = () => <h2>SurveyNew</h2>;
 
 class App extends Component {
