c1fe10d7daf9af68f58f8aa8c8bb30ea220713d9
Reza <rzgholizadeh@gmail.com>
2020-03-13 01:06:38 -0600

    feat: Parse the notification object from webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,5 @@
 const _ = require("lodash");
-const Path = require("path-parser");
+const { Path } = require("path-parser");
 const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
@@ -15,8 +15,19 @@ module.exports = app => {
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    console.log(req.body);
-    res.send({});
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path("/api/surveys/:surveyId/:choice");
+      const match = p.test(pathname);
+      if (match) {
+        return {
+          email,
+          surveyId: match.surveyId,
+          choice: match.choice
+        };
+      }
+    });
+    console.log(events);
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
